38c89378df4bf48bed2ea5a90326144d0d601bbb
Maxim M. Kaganovich <fiiqlae@gmail.com>
2020-08-01 23:11:35 +0300

    feat: add drink fragment ui implementation, viewmodel template implementation

--- app/src/main/java/com/mishaismenska/hackatonrsschoolapp/ui/AddDrinkFragment.kt
+++ app/src/main/java/com/mishaismenska/hackatonrsschoolapp/ui/AddDrinkFragment.kt
@@ -5,15 +5,49 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.mishaismenska.hackatonrsschoolapp.App
 import com.mishaismenska.hackatonrsschoolapp.R
+import com.mishaismenska.hackatonrsschoolapp.databinding.FragmentAddDrinkBinding
+import com.mishaismenska.hackatonrsschoolapp.viewmodels.AddDrinkViewModel
+import javax.inject.Inject
 
 class AddDrinkFragment : Fragment() {
+
+    private lateinit var binding: FragmentAddDrinkBinding
+
+    @Inject
+    lateinit var viewModel: AddDrinkViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_add_drink, container, false)
+        binding = FragmentAddDrinkBinding.inflate(inflater, container, false)
+
+        val drinkTypes = resources.getStringArray(R.array.drink_types)
+        binding.typeInput.setAdapter(
+            NoFilterAdapter(
+                requireContext(),
+                R.layout.drink_type_dropdown_item,
+                drinkTypes
+            )
+        )
+        binding.volumeInput.setText(drinkTypes.last())
+        val volumes = resources.getStringArray(R.array.volume_names)
+        binding.volumeInput.setAdapter(
+            NoFilterAdapter(
+                requireContext(),
+                R.layout.drink_type_dropdown_item,
+                volumes
+            )
+        )
+        binding.volumeInput.setText(volumes.last())
+        (requireActivity().application as App).appComponent.inject(this)
+        binding.goButton.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.main_fragment_container, MainFragment()).commit()
+        }
+        return binding.root
     }
 
 }
