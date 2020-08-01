40d4e3763764467f32c7d12891582369e9c1bb5f
Maxim M. Kaganovich <fiiqlae@gmail.com>
2020-08-01 20:47:06 +0300

    fix: rollback to origin

--- app/src/main/java/com/mishaismenska/hackatonrsschoolapp/ui/MainActivity.kt
+++ app/src/main/java/com/mishaismenska/hackatonrsschoolapp/ui/MainActivity.kt
@@ -6,11 +6,8 @@ import com.mishaismenska.hackatonrsschoolapp.R
 
 class MainActivity : AppCompatActivity() {
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction().replace(R.id.q, MainFragment()).commit()
     }
 }
