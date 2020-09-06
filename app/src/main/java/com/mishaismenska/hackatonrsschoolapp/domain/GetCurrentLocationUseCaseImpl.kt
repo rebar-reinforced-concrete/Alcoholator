75ceceafd96b66cb0e9f589c7ade7fe993d2258e
Maxim M. Kaganovich <fiiqlae@gmail.com>
2020-09-06 13:16:51 +0300

    current location fix

--- app/src/main/java/com/mishaismenska/hackatonrsschoolapp/domain/GetCurrentLocationUseCaseImpl.kt
+++ app/src/main/java/com/mishaismenska/hackatonrsschoolapp/domain/GetCurrentLocationUseCaseImpl.kt
@@ -1,25 +1,32 @@
 package com.mishaismenska.hackatonrsschoolapp.domain
 
-import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
-import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
 import android.location.LocationManager
-import androidx.core.app.ActivityCompat
+import android.util.Log
+import androidx.core.content.ContextCompat.getSystemService
 import com.mishaismenska.hackatonrsschoolapp.domain.interfaces.GetCurrentLocationUseCase
 import com.mishaismenska.hackatonrsschoolapp.presentation.models.LocationUIModel
 import java.time.LocalDateTime
 import javax.inject.Inject
 
+
 class GetCurrentLocationUseCaseImpl @Inject constructor() :
-    GetCurrentLocationUseCase {
+    GetCurrentLocationUseCase, LocationListener {
     @SuppressLint("MissingPermission") // we are requesting permissions
     override fun getCurrentLocation(locationManager: LocationManager): LocationUIModel {
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000L, 500.0f, this)
         val locationGPS = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
         return LocationUIModel(
             LocalDateTime.now().toString(),
             locationGPS!!.longitude,
             locationGPS.latitude
         )
+    }
+
+    override fun onLocationChanged(p0: Location) {
+        Log.d("LOCATION", p0.toString())
     }
 }
